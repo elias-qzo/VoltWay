416e6fa935883affc5577c071ee6bf5bf744c162
elias-qzo <eliascuzeau@gmail.com>
2025-01-19 17:12:15 +0100

    Itinerary html and css

--- web/public/scripts/main.js
+++ web/public/scripts/main.js
@@ -1,1 +1,20 @@
-console.log("Hello world");
+const map = L.map('map').setView([46.603354, 1.888334], 6);
+
+// Ajouter les tuiles OpenStreetMap
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// Ajouter un trajet avec Leaflet Routing Machine
+L.Routing.control({
+    waypoints: [
+        L.latLng(48.8566, 2.3522), // Point de départ : Paris
+        L.latLng(43.6047, 1.4442)  // Point d'arrivée : Toulouse
+        ],
+    routeWhileDragging: false,
+    show: false,
+    addWaypoints: false,
+    lineOptions: {
+        styles: [{ color: '#c71f1f', weight: 3 }]
+    }
+}).addTo(map);
