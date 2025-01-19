328bb6acf40caa09dc2f2deba6ddd727e5b527ee
elias-qzo <116678729+elias-qzo@users.noreply.github.com>
2025-01-19 19:21:37 +0100

    Location itinerary api route

--- api/app.py
+++ api/app.py
@@ -1,18 +1,71 @@
-from flask import Flask
+import requests
+from flask import Flask, request
 from flask_restful import Api, Resource
+from flask_cors import CORS
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
-PORT = os.getenv("PORT")
+PORT = os.getenv("PORT", 6060)
+
 app = Flask(__name__)
 api = Api(app)
+CORS(app, origins="http://localhost:3000")
+
+#
+# RESSOURCES
+#
 
 class HelloWorld(Resource):
     def get(self):
         return {"message": "Hello, World!"}
+    
+def get_lat_lon(location):
+        params = {
+            "q": location,
+            "format": "json",
+            "limit": 1
+        }
+        headers = {
+            "User-Agent": "VoltWay/1.0"
+        }
 
-api.add_resource(HelloWorld, "/")
+        response = requests.get("https://nominatim.openstreetmap.org/search", params=params, headers=headers)
+
+        if response.status_code == 200:
+            data = {
+                "lat": response.json()[0]["lat"],
+                "lon": response.json()[0]["lon"]
+            }
+            print(data)
+            return data
+        else:
+            print(f"Erreur {response.status_code}: {response.reason}")
+            return None
+
+class Itinerary(Resource):
+    def get(self):
+        origin = request.args.get('origin')
+        destination = request.args.get('destination')
+        
+        if not origin or not destination:
+            return {"error": "Missing origin or destination"}, 400
+        
+        origin_coord = get_lat_lon(origin)
+        destination_coord = get_lat_lon(destination)
+
+        if not origin_coord or not destination_coord:
+            return {"error": "Cannot get coord from localisation"}, 400
+
+        return {"origin": origin_coord, "destination": destination_coord}, 200
+
+#
+# ROUTES
+#
+
+api.add_resource(HelloWorld, "/helloworld")
+api.add_resource(Itinerary, "/itinerary")
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=PORT)
--- web/public/scripts/main.js
+++ web/public/scripts/main.js
@@ -1,20 +1,48 @@
-const map = L.map('map').setView([46.603354, 1.888334], 6);
+const api = axios.create({
+    baseURL: 'http://127.0.0.1:6060',
+    timeout: 5000,
+});
 
-// Ajouter les tuiles OpenStreetMap
+const map = L.map('map').setView([46.603354, 1.888334], 6);
+const originInput = document.getElementById("origin-input")
+const destinationInput = document.getElementById("destination-input")
+const travelButton = document.getElementById("travel-button")
+
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// Ajouter un trajet avec Leaflet Routing Machine
-L.Routing.control({
-    waypoints: [
-        L.latLng(48.8566, 2.3522), // Point de départ : Paris
-        L.latLng(43.6047, 1.4442)  // Point d'arrivée : Toulouse
-        ],
-    routeWhileDragging: false,
-    show: false,
-    addWaypoints: false,
-    lineOptions: {
-        styles: [{ color: '#c71f1f', weight: 3 }]
+function setTravel(origin, destination) {
+    if (window.currentRouteControl) {
+        map.removeControl(window.currentRouteControl);
     }
-}).addTo(map);
+
+    window.currentRouteControl = L.Routing.control({
+        waypoints: [
+            L.latLng(origin["lat"], origin["lon"]),
+            L.latLng(destination["lat"], destination["lon"])
+        ],
+        routeWhileDragging: false,
+        show: false, 
+        addWaypoints: false,
+        lineOptions: {
+            styles: [{ color: '#4275f5', weight: 3 }]
+        }
+    }).addTo(map);
+}
+
+
+travelButton.addEventListener("click",function(){
+    let params = {
+        "origin": originInput.value,
+        "destination": destinationInput.value
+    }
+    api.get('/itinerary', { params })
+    .then(response => {
+        console.log(response.data);
+        setTravel(response.data["origin"],response.data["destination"])
+    })
+    .catch(error => {
+        console.error("Error :", error);
+    });
+})
