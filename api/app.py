caf89acb9503700d7f98c2d7653d6395feea9cc2
elias-qzo <eliascuzeau@gmail.com>
2025-01-20 15:56:56 +0100

    Car selection

--- api/app.py
+++ api/app.py
@@ -7,6 +7,8 @@ import os
 
 load_dotenv()
 PORT = os.getenv("PORT", 6060)
+CHARGETRIP_CLIENT_ID = os.getenv("CHARGETRIP_CLIENT_ID", "NONE")
+CHARGETRIP_APP_ID = os.getenv("CHARGETRIP_APP_ID", "NONE")
 
 app = Flask(__name__)
 api = Api(app)
@@ -37,7 +39,6 @@ def get_lat_lon(location):
                 "lat": response.json()[0]["lat"],
                 "lon": response.json()[0]["lon"]
             }
-            print(data)
             return data
         else:
             print(f"Erreur {response.status_code}: {response.reason}")
@@ -59,12 +60,81 @@ class Itinerary(Resource):
 
         return {"origin": origin_coord, "destination": destination_coord}, 200
 
+def get_vehicles_data(brand):
+    query = """
+    query vehicleListAll($brand: String!) {
+        vehicleList(search: $brand) {
+            id
+            naming {
+                make
+                model
+            }
+            connectors {
+                standard
+                time
+            }
+            range {
+                chargetrip_range {
+                    best
+                    worst
+                }
+            }
+            media {
+                image {
+                    url
+                }
+            }
+        }
+    }
+    """
+    variables = {
+        "brand": brand
+    }
+    headers = {
+        "User-Agent": "VoltWay/1.0",
+        "Content-Type": "application/json",
+        "x-client-id": CHARGETRIP_CLIENT_ID,
+        "x-app-id": CHARGETRIP_APP_ID
+    }
+    payload = {
+        "query": query,
+        "variables": variables
+    }
+
+    response = requests.post("https://api.chargetrip.io/graphql", json=payload, headers=headers)
+
+    if response.status_code == 200:
+        vehicles = []
+        data = response.json()
+        for vehicle in data["data"]["vehicleList"]:
+            v = {}
+            v['brand'] = vehicle['naming']['make']
+            v['model'] = vehicle['naming']['model']
+            v['autonomy'] = vehicle['range']['chargetrip_range']['worst']
+            v['image'] = vehicle['media']['image']['url']
+            charge_time = vehicle['connectors'][0]['time']
+            for i in range(1, len(vehicle['connectors'])):
+                if charge_time > vehicle['connectors'][i]['time']:
+                    charge_time = vehicle['connectors'][i]['time']
+            v['charge-time'] = charge_time
+            vehicles.append(v)
+        return vehicles
+    else:
+        print(f"Erreur {response.status_code}: {response.reason}")
+        return None
+
+class Vehicles(Resource):
+    def get(self):
+        brand = request.args.get('brand')
+        return get_vehicles_data(brand)
+
 #
 # ROUTES
 #
 
 api.add_resource(HelloWorld, "/helloworld")
 api.add_resource(Itinerary, "/itinerary")
+api.add_resource(Vehicles, "/vehicles")
 
 
 if __name__ == "__main__":
--- web/public/scripts/main.js
+++ web/public/scripts/main.js
@@ -46,3 +46,79 @@ travelButton.addEventListener("click",function(){
         console.error("Error :", error);
     });
 })
+
+
+let carBrandSelect = document.getElementById("car-brand")
+let carModelSelect = document.getElementById("car-model")
+carModelSelect.style.display = "none"
+let carData = []
+
+carBrandSelect.addEventListener("change", function () {
+    if(carBrandSelect.value == "default"){
+        carModelSelect.style.display = "none"
+    } else {
+        carModelSelect.style.display = "flex";
+        let params = {
+            "brand": carBrandSelect.value,
+        }
+
+        api.get('/vehicles', { params })
+        .then(response => {
+            carData = response.data
+            initModelSelect()
+            console.log(response.data);
+        })
+        .catch(error => {
+            console.error("Error :", error);
+        });
+    }
+});
+
+carModelSelect.addEventListener("change", function () {
+    if(carModelSelect.value == "default"){
+        carInfosContainer.style.display = "none"
+    } else {
+        initCarInfo(carData[carModelSelect.value])
+    }
+})
+
+function create(tagName, container, text = null, classs = null, id = null) {
+    let element = document.createElement(tagName)
+    container.appendChild(element)
+    if (text)
+        element.appendChild(document.createTextNode(text))
+    if (classs)
+        element.classList.add(classs)
+    if (id)
+        element.id = id
+    return element
+}
+
+function initModelSelect(){
+    while (carModelSelect.firstChild) {
+        carModelSelect.removeChild(carModelSelect.firstChild);
+    }
+    create("option",carModelSelect,"Choississez un model",id="default")
+    for(let i = 0; i < carData.length; i++){
+        let model = create("option",carModelSelect,carData[i]['model'])
+        model.value = i
+    }
+}
+
+let carInfosContainer = document.getElementById("car-infos")
+carInfosContainer.style.display = "none"
+
+let carImage = document.getElementById("car-image")
+let carModelInfo = document.getElementById("car-model-info")
+let carAutonomyInfo = document.getElementById("car-autonomy-info")
+let carChargetimeInfo = document.getElementById("car-chargetime-info")
+
+function initCarInfo(carData){
+    carInfosContainer.style.display = "inline"
+    carImage.src = carData['image']
+    carModelInfo.innerHTML = `${carData['brand']} ${carData['model']}`
+    carAutonomyInfo.innerHTML = `Autonomie : ${carData['autonomy']} mn`
+    carChargetimeInfo.innerHTML = `Temps de charge : ${carData['charge-time']} mn`
+}
+
+carModelSelect
